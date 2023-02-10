350df6f8b3ad803d6476dbc5a8857b61f4f04ba8
Isabella Gard <2667477g@student.gla.ac.uk>
2023-02-10 12:23:38 +0000

    Chapter 9 exercises final

--- rango/views.py
+++ rango/views.py
@@ -34,6 +34,7 @@ def index(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     #render the response and send it back!
+    request.session.set_test_cookie()
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
@@ -78,6 +79,7 @@ def show_category(request, category_name_slug):
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -101,6 +103,7 @@ def add_category(request):
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -232,7 +235,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 # Use the login_required() decorator to ensure only those logged in can
 # access the view.
